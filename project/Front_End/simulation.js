82e0d01a11c494e2e2265ca2717e03d16b31a0ea
Karl07Yammine <karl.yammine1+github@gmail.com>
2024-10-06 18:49:17 +0300

    now displaying data

--- project/Front_End/simulation.js
+++ project/Front_End/simulation.js
@@ -50,4 +50,38 @@ function playBackward() {
 }
 
 
+const jsonData = [
+    {"Second":"1","Time":"1:23","Speed":"970.3","Density":"1.36","IMF - Bt":"7.34","IMF - Bz":"0.96"},
+    {"Second":"2","Time":"1:23","Speed":"992.2","Density":"1.93","IMF - Bt":"7.62","IMF - Bz":"-5.17"},
+    {"Second":"3","Time":"1:24","Speed":"967.6","Density":"2.19","IMF - Bt":"8.16","IMF - Bz":"-5.71"},
+    {"Second":"4","Time":"1:24","Speed":"959.8","Density":"2.07","IMF - Bt":"7.71","IMF - Bz":"-2.56"},
+    {"Second":"5","Time":"1:25","Speed":"938.7","Density":"6.97","IMF - Bt":"6.71","IMF - Bz":"-4.28"},
+    {"Second":"6","Time":"1:25","Speed":"916.5","Density":"4.16","IMF - Bt":"8.24","IMF - Bz":"-3.38"},
+    {"Second":"7","Time":"1:26","Speed":"932.8","Density":"10.18","IMF - Bt":"6.27","IMF - Bz":"-4.28"},
+    {"Second":"8","Time":"1:26","Speed":"887.3","Density":"3.52","IMF - Bt":"7.22","IMF - Bz":"4.21"},
+    {"Second":"9","Time":"1:27","Speed":"905.8","Density":"8.13","IMF - Bt":"6.66","IMF - Bz":"-3.74"},
+    {"Second":"10","Time":"1:27","Speed":"915.7","Density":"10.56","IMF - Bt":"6.39","IMF - Bz":"1.79"},
+    {"Second":"11","Time":"1:28","Speed":"923.6","Density":"4.5","IMF - Bt":"8.12","IMF - Bz":"1.68"},
+    {"Second":"12","Time":"1:28","Speed":"893.6","Density":"5.83","IMF - Bt":"7.63","IMF - Bz":"3.14"},
+    {"Second":"13","Time":"1:29","Speed":"888.9","Density":"3.82","IMF - Bt":"6.55","IMF - Bz":"3.75"},
+    {"Second":"14","Time":"1:29","Speed":"887.1","Density":"3.37","IMF - Bt":"6.11","IMF - Bz":"2.9"},
+    {"Second":"15","Time":"1:30","Speed":"869.7","Density":"4.21","IMF - Bt":"5.74","IMF - Bz":"2.27"},
+    {"Second":"16","Time":"1:30","Speed":"874","Density":"3.29","IMF - Bt":"5.72","IMF - Bz":"1.97"}
+];
 
+const totalIntervals = jsonData.length;
+
+video.addEventListener('timeupdate', () => {
+    const currentTime = video.currentTime;
+    const duration = video.duration;
+    const intervalIndex = Math.floor((currentTime / duration) * totalIntervals);
+    
+    if (intervalIndex < totalIntervals && intervalIndex >= 0) {
+        const data = jsonData[intervalIndex];
+        document.getElementById('time').textContent = `Time: ${data.Time}`;
+        document.getElementById('speed').textContent = `Speed: ${data.Speed} km/s`;
+        document.getElementById('density').textContent = `Density: ${data.Density} cm³`;
+        document.getElementById('imf-bt').textContent = `IMF - Bt: ${data['IMF - Bt']}`;
+        document.getElementById('imf-bz').textContent = `IMF - Bz: ${data['IMF - Bz']}`;
+    }
+});
